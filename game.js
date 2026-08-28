cdde8599b11878a7871c5017cc08da23ac7b5c3a
ychole7 <yeonchil.kim@gmail.com>
2026-08-28 15:19:35 +0900

    Add files via upload

--- game.js
+++ game.js
@@ -243,6 +243,7 @@
         if(v===null){ el.style.visibility='hidden'; }
         else { el.style.visibility='visible'; el.dataset.v=v; el.innerHTML = renderSymbolHTML(stage, v); }
         el.classList.remove('selected','popping');
+        el.classList.toggle('imgTile', stage.type==='img6');
       }
     }
   }
