c7754d35f41398c698a5893ee75e0527c57a4216
ychole7 <yeonchil.kim@gmail.com>
2026-08-31 15:00:35 +0900

    Add files via upload

--- game.js
+++ game.js
@@ -717,13 +717,6 @@
     failOverlay.classList.remove('show');
     newStageBoard();
   });
-  document.getElementById('giveUpBtn').addEventListener('click', ()=>{
-    if(hearts<=0){ showToast('하트가 부족해요! 상단 + 버튼으로 충전해보세요'); return; }
-    spendHeart();
-    updateHud();
-    failOverlay.classList.remove('show');
-    newStageBoard();
-  });
 
   gridEl.addEventListener('pointerdown', (e)=>{
     const cell = cellFromPoint(e.clientX,e.clientY);
